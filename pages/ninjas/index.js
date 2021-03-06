795bb07cee92bd4adb2c15d9b88c5b2be857cd6f
Artur Tavares <artur@loba.pt>
2021-03-06 19:48:48 +0000

    Tutorial End

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,8 +1,9 @@
-import Head from "next/head";
-import styles from "../../styles/Ninjas.module.css";
+import Head from 'next/head';
+import styles from '../../styles/Ninjas.module.css';
+import Link from 'next/link';
 
 export const getStaticProps = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await res.json();
   return {
     props: {
@@ -21,11 +22,11 @@ const Ninjas = ({ ninjas }) => {
       <div>
         <h1>All Ninjas</h1>
         {ninjas.map((ninja) => (
-          <div key={ninja.id}>
+          <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
             <a className={styles.single}>
               <h3>{ninja.name}</h3>
             </a>
-          </div>
+          </Link>
         ))}
       </div>
     </>
