da3c7b047f3aea17b5f8cb3f9b8552e8d626ab83
AfterJoseph <tavaresart11@gmail.com>
2021-03-06 18:41:04 +0000

    First Commit

---
+++ pages/about.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninbja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque
+          nesciunt doloremque explicabo odio corporis, ipsum dolor ab accusamus
+          debitis veritatis expedita quibusdam magnam libero, fuga,
+          exercitationem minus unde ullam esse.
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque
+          nesciunt doloremque explicabo odio corporis, ipsum dolor ab accusamus
+          debitis veritatis expedita quibusdam magnam libero, fuga,
+          exercitationem minus unde ullam esse.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
